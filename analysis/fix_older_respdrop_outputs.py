82637e8def1f435b9b11ffbc18145f91b7d2585b
Ramakrishna Padmanabhan <ramapad@caida.org>
2020-03-19 00:19:10 -0400

    Scripts for fixing output directories for new Zeusping pipeline

--- analysis/fix_older_respdrop_outputs.py
+++ analysis/fix_older_respdrop_outputs.py
@@ -20,6 +20,7 @@ except subprocess.CalledProcessError:
     sys.stderr.write("Mkdir failed for {0}; exiting\n".format(mkdir_cmd) )
     sys.exit(1)
 
+
 round_tstart = int(sys.argv[3])
 round_tend = int(sys.argv[4])
 
@@ -37,16 +38,16 @@ def run_mv(temp_tstart, temp_tend):
         sys.exit(1)
 
     # mv_cmd = 'mv {0}/{1}_to_{2}/resps_per_addr.gz {3}/{1}_to_{2}/'.format(src_dir, temp_tstart, temp_tend, dst_dir)
-    mv_cmd = 'mv {0}/{1}_to_{2}/process_round.log.gz {3}/{1}_to_{2}/'.format(src_dir, temp_tstart, temp_tend, dst_dir)
-    # sys.stderr.write("{0}\n".format(mv_cmd) )
+    # mv_cmd = 'mv {0}/responsive_and_dropout_addrs/{1}_to_{2}.gz {3}/{1}_to_{2}/dropout_resp_antidropout_addrs.gz'.format(src_dir, temp_tstart, temp_tend, dst_dir)
+    mv_cmd = 'mv {0}/{1}_to_{2}/dropout_resp_antidropout_addrs.gz {3}/{1}_to_{2}/'.format(src_dir, temp_tstart, temp_tend, dst_dir)
+    sys.stderr.write("{0}\n".format(mv_cmd) )
     args = shlex.split(mv_cmd)
 
     try:
         subprocess.check_call(args)
     except subprocess.CalledProcessError:
-        sys.stderr.write("mv failed for f {0}; exiting\n".format(f) )
+        sys.stderr.write("mv failed for cmd {0}; exiting\n".format(mv_cmd) )
         sys.exit(1)
-
 
 
 file_ct = 0
