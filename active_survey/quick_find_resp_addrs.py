3ed336236863b820d6728ee8723b769385cea7e7
Ramakrishna Padmanabhan <ramapad@caida.org>
2020-08-19 10:32:47 -0700

    Committing a poor intermediate version for reading ISI hitlist and determining country-level AS-level active probing response rate. This script worked for Iran but fails for the US since it requires too much memory for US addresses that respond to active probes

--- active_survey/quick_find_resp_addrs.py
+++ active_survey/quick_find_resp_addrs.py
@@ -8,9 +8,7 @@ from collections import namedtuple
 import wandio
 import datetime
 
-addr_filename = sys.argv[1]
-netacq_date = sys.argv[2]
-mode = sys.argv[3] # Mode can be 'isi' for ISI Hitlist, 'zmap' for Zmap scan, 'zeus' for ZeusPing
+mode = sys.argv[1] # Mode can be 'isi' for ISI Hitlist, 'zmap' for Zmap scan, 'zeus' for ZeusPing
 
 # Load pyipmeta in order to perform county lookups per address
 # provider_config_str = "-b /data/external/netacuity-dumps/Edge-processed/{0}.netacq-4-blocks.csv.gz -l /data/external/netacuity-dumps/Edge-processed/{0}.netacq-4-locations.csv.gz -p /data/external/netacuity-dumps/Edge-processed/{0}.netacq-4-polygons.csv.gz -t /data/external/gadm/polygons/gadm.counties.v2.0.processed.polygons.csv.gz -t /data/external/natural-earth/polygons/ne_10m_admin_1.regions.v3.0.0.processed.polygons.csv.gz".format(netacq_date)
@@ -56,6 +54,54 @@ def get_zeus_addrs(addr_filename, annotated_op_fname):
     return resp_addrs, unresp_addrs
 
 
+def get_isi_addrs(all_addrs, ip_to_as):
+
+    resp_addrs = set()
+    line_ct = 0
+
+    NUM_HISTS = 2 # How many historical censuses we will consider to determine if an address was respsonsive
+    
+    for line in sys.stdin:
+
+        line_ct += 1
+
+        if line_ct == 1:
+            continue
+
+        if line_ct%1000000 == 0:
+            sys.stderr.write("{0} isi_hitlist lines read at {1}\n".format(line_ct, str(datetime.datetime.now() ) ) )
+        
+        # if line_ct > 1000:
+        #     sys.exit(1)
+        
+        parts = line.strip().split()
+
+        ip = parts[1].strip()
+
+        hist = parts[2].strip()
+        hist = hist[-1] # NOTE: Hack to make this faster temporarily
+
+        hex_int = int(hist, 16)
+
+        resp = 0
+
+        for i in range(NUM_HISTS):
+            if hex_int & 1 == 1:
+                resp = 1
+
+            hex_int = hex_int >> 1
+
+        # print ip, hist, hex_int, resp
+
+        if resp == 1:
+            resp_addrs.add(ip)
+
+
+    unresp_addrs = all_addrs - resp_addrs
+
+    return resp_addrs, unresp_addrs
+        
+
 def update_region_asn_to_status(region_asn_to_status, addrs, mode):
 
     for addr in addrs:
@@ -66,6 +112,10 @@ def update_region_asn_to_status(region_asn_to_status, addrs, mode):
                 usstate = ip_to_usstate[addr]
             else:
                 usstate = '-1'
+
+        elif mode == 'isi':
+            # TODO: Hack to just ignore usstate for now
+            usstate = 'NA'
 
         # Find ASN
         if addr in ip_to_as:
@@ -85,6 +135,9 @@ def update_region_asn_to_status(region_asn_to_status, addrs, mode):
 # NOTE: When running this for ISI's hitlist and/or Zmap scans, we'll need some way to get the ASN of each address
 
 if mode == 'zeus':
+
+    addr_filename = sys.argv[1]
+    netacq_date = sys.argv[2]
 
     ip_to_as_file = sys.argv[4] # Each line of this file is a path to an AS file for a U.S. state    
     ip_to_usstate = {}
@@ -125,13 +178,50 @@ if mode == 'zeus':
     annotated_op_fname = "{0}_annotated".format(addr_filename)
     resp_addrs, unresp_addrs = get_zeus_addrs(addr_filename, annotated_op_fname)
 
+    
+elif mode == 'isi':
+    
+    ip_to_as_file = sys.argv[2]
+    # ip_to_usstate = {}
+    ip_to_as = {}
 
+    all_addrs = set() # Set that contains all addresses in Netacuity + pfx2as for a given country that we're exploring
+
+    ip_to_as_fp = wandio.open(ip_to_as_file)
+    line_ct = 0
+    for line in ip_to_as_fp:
+
+        line_ct += 1
+
+        if line_ct%1000000 == 0:
+            sys.stderr.write("{0} ip_to_as lines read at {1}\n".format(line_ct, str(datetime.datetime.now() ) ) )
+
+        parts = line.strip().split('|')
+
+        if (len(parts) != 2):
+            continue
+
+        addr = parts[0].strip()
+        asn = parts[1].strip()
+
+        ip_to_as[addr] = asn
+
+        all_addrs.add(addr)
+
+    sys.stderr.write("Done reading ip_to_as at {0}\n".format(str(datetime.datetime.now() ) ) )
+
+    resp_addrs, unresp_addrs = get_isi_addrs(all_addrs, ip_to_as)
+    
+    
 region_asn_to_status = {"resp" : {}, "unresp" : {}}
 # Maybe this can eventually even have county-level data. For now, I will begin with state-level data.
 update_region_asn_to_status(region_asn_to_status["resp"], resp_addrs, mode)
 update_region_asn_to_status(region_asn_to_status["unresp"], unresp_addrs, mode)
 
-op_fname = "{0}_regionasn_to_status".format(addr_filename)
+if mode == 'zeus':
+    op_fname = "{0}_regionasn_to_status".format(addr_filename)
+elif mode == 'isi':
+    op_fname = "temp"
 op_fp = open(op_fname, "w")
 
 for usstate in region_asn_to_status["resp"]:
