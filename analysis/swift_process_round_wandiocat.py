099c10a040cef0c84ad198fb4ade15f1c938c9f2
Ramakrishna Padmanabhan <ramapad@caida.org>
2021-01-09 14:18:51 -0800

    Code refactoring to allow running of the line profile

--- analysis/swift_process_round_wandiocat.py
+++ analysis/swift_process_round_wandiocat.py
@@ -10,6 +10,7 @@ import datetime
 import json
 from collections import defaultdict
 import io
+import wandio
 
 
 def setup_stuff():
@@ -27,6 +28,32 @@ def setup_stuff():
     return op_log_fp
 
 
+def find_potential_files():
+
+    # NOTE: We may want to obtain the files for the previous 10-minute round too but *not* the next 10-minute round.
+    # Suppose the tstamps in all VP files are exactly at 600s. Then we would only need to process this round num's files. This is because each file will contain the next 600s worth of pings. These are exactly the pings that we needed to have processed.
+    # Suppose the tstamps in all VP files are 601s. Then we would need to process 599s worth of pings from all these files, but discard the last second. When we process the *next round*, we should process the last second from this round (otherwise the next round's first second would never get processed).
+    # Suppose the tstamps in all VP files are 599s. Then we would have needed to process 599s (600 to 1199s) worth of pings but we would be discarding all of these if we are not processing the previous round. 
+
+    # Find the hour edge of this required round
+    round_tstart_dt = datetime.datetime.utcfromtimestamp(round_tstart)
+
+    # NOTE: Perhaps replace swift_list_cmd with wandio.swift.list? I don't think it'll buy us that much additional efficiency though...
+    # elems = wandio.swift.list('zeusping-warts', )
+
+    # NOTE: If we are processing the previous round as well, may need to modify the swift_list_cmd to ensure that previous round's files will also be included. If this round is 00:00 but previous round is from the previous day at 23:50, we'll need to modify the swift list command. We'll probably just have to call the swift_list_cmd twice (once for round_tstart_dt.stuff and another for prev_round_tstart_dt.stuff, where prev_round = round - 600)
+    swift_list_cmd = 'swift list zeusping-warts -p datasource=zeusping/campaign={0}/year={1}/month={2}/day={3}/hour={4}/'.format(campaign, round_tstart_dt.year, round_tstart_dt.strftime("%m"), round_tstart_dt.strftime("%d"), round_tstart_dt.strftime("%H"))
+    # print swift_list_cmd
+    args = shlex.split(swift_list_cmd)
+    try:
+        potential_files = subprocess32.check_output(args)
+    except subprocess32.CalledProcessError:
+        sys.stderr.write("Swift list failed for {0}; exiting\n".format(swift_list_cmd) )
+        sys.exit(1)
+
+    return potential_files
+
+        
 def update_addr_to_resps(fname, addr_to_resps):
 
     wandiocat_cmd = './swift_wrapper.sh swift://zeusping-warts/{0}'.format(fname)
@@ -82,8 +109,8 @@ def update_addr_to_resps(fname, addr_to_resps):
             dst = data['dst']
 
             # NOTE: defaultdict should take care of the following... let's see if this gets us savings
-            # if dst not in addr_to_resps:
-            #     addr_to_resps[dst] = [0, 0, 0, 0, 0]
+            if dst not in addr_to_resps:
+                addr_to_resps[dst] = [0, 0, 0, 0, 0]
             addr_to_resps[dst][0] += 1 # 0th index is sent packets
 
             # pinged_ts = data['start']['sec']
@@ -116,80 +143,70 @@ def update_addr_to_resps(fname, addr_to_resps):
     #     line_ct += 1
     
 
+def write_addr_to_resps(addr_to_resps, processed_op_dir, round_tstart, round_tend, op_log_fp):
+    if len(addr_to_resps) > 0:
 
-############## Main begins here #####################
+        ping_aggrs_fp = wandio.open('{0}/{1}_to_{2}/resps_per_addr.gz'.format(processed_op_dir, round_tstart, round_tend), 'w')
+        dst_ct = 0
+        for dst in addr_to_resps:
 
+            dst_ct += 1
+            this_d = addr_to_resps[dst]
+            ping_aggrs_fp.write("{0} {1} {2} {3} {4} {5}\n".format(dst, this_d[0], this_d[1], this_d[2], this_d[3], this_d[4] ) )
+
+        ping_aggrs_fp.close()
+
+    op_log_fp.write("Done with round {0}_to_{1} at: {2}\n".format(round_tstart, round_tend, str(datetime.datetime.now() ) ) )
+
+    
+def main():
+    ############## Main begins here #####################
+
+
+    reqd_round_num = int(round_tstart)/600
+
+    # Find all files that we *may* need to process
+    potential_files = find_potential_files()
+
+    num_pot_files = len(potential_files)
+    is_setup_done = 0 # By default, we wouldn't create directories or output files; not unless there are actually warts files to process for this round. This flag keeps track of whether we've "setup" (which we would only have done had we encountered useful warts files).
+    # addr_to_resps = {}
+    addr_to_resps = defaultdict(lambda : [0, 0, 0, 0, 0])
+
+    # TODO: Think about whether we would need to read files generated a minute or two before/after current round
+    if (num_pot_files > 0):
+
+        for fname in potential_files.strip().split('\n'):
+            # print fname
+            parts = fname.strip().split('.warts.gz')
+            # print parts
+            file_ctime = parts[0][-10:]
+            # print file_ctime
+
+            round_num = int(file_ctime)/600
+
+            if round_num == reqd_round_num:
+                # We found a warts file that belongs to this round and needs to be processed
+
+                if is_setup_done == 0:
+                    op_log_fp = setup_stuff()
+                    is_setup_done = 1
+
+                update_addr_to_resps(fname, addr_to_resps)
+
+                op_log_fp.write("Done reading {0} at: {1}\n".format(fname, str(datetime.datetime.now() ) ) )
+
+
+    # If there is anything in addr_to_resps, let's write it.
+    write_addr_to_resps(addr_to_resps, processed_op_dir, round_tstart, round_tend, op_log_fp)
+
+
+############## Read command line args and call main() #####################
 campaign = sys.argv[1] # CO_VT_RI/FL/iran_addrs
-
 round_tstart = int(sys.argv[2])
+
 round_tend = round_tstart + 600
-reqd_round_num = int(round_tstart)/600
-
-# NOTE: We may want to obtain the files for the previous 10-minute round too but *not* the next 10-minute round.
-# Suppose the tstamps in all VP files are exactly at 600s. Then we would only need to process this round num's files. This is because each file will contain the next 600s worth of pings. These are exactly the pings that we needed to have processed.
-# Suppose the tstamps in all VP files are 601s. Then we would need to process 599s worth of pings from all these files, but discard the last second. When we process the *next round*, we should process the last second from this round (otherwise the next round's first second would never get processed).
-# Suppose the tstamps in all VP files are 599s. Then we would have needed to process 599s (600 to 1199s) worth of pings but we would be discarding all of these if we are not processing the previous round. 
-
 # NOTE: Change output dir for each test!
 processed_op_dir = '/scratch/zeusping/data/processed_op_{0}_test'.format(campaign)
 
-# Find current working directory
-this_cwd = os.getcwd()
-
-# Find the hour edge of this required round
-round_tstart_dt = datetime.datetime.utcfromtimestamp(round_tstart)
-
-# NOTE: Perhaps replace swift_list_cmd with wandio.swift.list? I don't think it'll buy us that much additional efficiency though...
-# elems = wandio.swift.list('zeusping-warts', )
-
-# NOTE: If we are processing the previous round as well, may need to modify the swift_list_cmd to ensure that previous round's files will also be included. If this round is 00:00 but previous round is from the previous day at 23:50, we'll need to modify the swift list command. We'll probably just have to call the swift_list_cmd twice (once for round_tstart_dt.stuff and another for prev_round_tstart_dt.stuff, where prev_round = round - 600)
-swift_list_cmd = 'swift list zeusping-warts -p datasource=zeusping/campaign={0}/year={1}/month={2}/day={3}/hour={4}/'.format(campaign, round_tstart_dt.year, round_tstart_dt.strftime("%m"), round_tstart_dt.strftime("%d"), round_tstart_dt.strftime("%H"))
-# print swift_list_cmd
-args = shlex.split(swift_list_cmd)
-try:
-    potential_files = subprocess32.check_output(args)
-except subprocess32.CalledProcessError:
-    sys.stderr.write("Swift list failed for {0}; exiting\n".format(swift_list_cmd) )
-    sys.exit(1)
-
-num_pot_files = len(potential_files)
-is_setup_done = 0 # By default, we wouldn't create directories or output files; not unless there are actually warts files to process for this round. This flag keeps track of whether we've "setup" (which we would only have done had we encountered useful warts files).
-# addr_to_resps = {}
-addr_to_resps = defaultdict(lambda : [0, 0, 0, 0, 0])
-
-# TODO: Think about whether we would need to read files generated a minute or two before/after current round
-if (num_pot_files > 0):
-
-    for fname in potential_files.strip().split('\n'):
-        # print fname
-        parts = fname.strip().split('.warts.gz')
-        # print parts
-        file_ctime = parts[0][-10:]
-        # print file_ctime
-
-        round_num = int(file_ctime)/600
-
-        if round_num == reqd_round_num:
-            # We found a warts file that belongs to this round and needs to be processed
-            
-            if is_setup_done == 0:
-                op_log_fp = setup_stuff()
-                is_setup_done = 1
-
-            update_addr_to_resps(fname, addr_to_resps)
-
-            op_log_fp.write("Done reading {0} at: {1}\n".format(fname, str(datetime.datetime.now() ) ) )
-
-
-# If there is anything in addr_to_resps, let's write it.
-if len(addr_to_resps) > 0:
-
-    ping_aggrs_fp = open('{0}/{1}_to_{2}/resps_per_addr'.format(processed_op_dir, round_tstart, round_tend), 'w')
-    dst_ct = 0
-    for dst in addr_to_resps:
-
-        dst_ct += 1
-        this_d = addr_to_resps[dst]
-        ping_aggrs_fp.write("{0} {1} {2} {3} {4} {5}\n".format(dst, this_d[0], this_d[1], this_d[2], this_d[3], this_d[4] ) )
-
-op_log_fp.write("Done with round {0}_to_{1} at: {2}\n".format(round_tstart, round_tend, str(datetime.datetime.now() ) ) )
+main()
