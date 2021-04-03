0a6510f9e1710524511c7b785a94557861b23340
Ramakrishna Padmanabhan <ramapad@caida.org>
2021-04-02 22:47:12 -0700

    Added generic function to return the list of files for a power company in sorted order between the specified start and endtimes

--- power/parse_power_outage_websites/power_outage_parser.py
+++ power/parse_power_outage_websites/power_outage_parser.py
@@ -4,8 +4,11 @@ import time
 import datetime
 import dateutil
 import calendar
+import re
+import glob
 
-MIN_THRESH = 1000
+STATE_MIN_THRESH = 10000
+COUNTY_MIN_THRESH = 1000
 GAP_THRESH = 86400
 
 
@@ -36,5 +39,38 @@ def update_regional_outages(ongoing_regional_outages, regional_outages, regionNa
             # It has been more than 30 minutes since the last outage. Write old outage and start new outage                           
             regional_outages.append({"start" : ongoing_regional_outages[regionName]["start"], "end" : ongoing_regional_outages[regionName]["last"], "custs" : ongoing_regional_outages[regionName]["custs"], "regionName" : regionName})
             ongoing_regional_outages[regionName] = {"start" : written_time, "last" : written_time, "custs" : customersAffected}
+
+
+def get_tstamp_to_fname(po_company, start_time, end_time, aggr, regex_str, tstamp_to_fname):
+
+    aggr_regex = re.compile(regex_str)
+
+    for hour_epoch in range(start_time, end_time, 3600):
+        hour_dt = datetime.datetime.utcfromtimestamp(hour_epoch)
+
+        fdir = '/scratch/zeusping/power_aug2020_to_mar2021/{0}/year={1}/month={2}/day={3}/hour={4}/*'.format(po_company, hour_dt.year, hour_dt.strftime("%m"), hour_dt.strftime("%d"), hour_dt.strftime("%H") )    
+        # fdir = '/scratch/zeusping/power/{0}/year={1}/month={2}/day={3}/hour={4}/'.format(po_company, hour_dt.year, hour_dt.strftime("%m"), hour_dt.strftime("%d"), hour_dt.strftime("%H") )
+
+        unsorted_files = glob.glob(fdir)
+
+        for fil in unsorted_files:
+            matched_stuff = aggr_regex.search(fil)
+
+            if matched_stuff is None:
+                # sys.stdout.write("Did not match: {0}\n".format(fil) )
+                continue
+
+            # sys.stdout.write("Matched: {0}\n".format(fil) )
+
+            this_tstamp = matched_stuff.group(1)
+            # sys.stdout.write("{0}\n".format(this_fname) )
+
+            tstamp_to_fname[this_tstamp] = fil
+
+        # sys.exit(1)
+
+    sorted_d = sorted(tstamp_to_fname.items(), key=lambda kv: kv[0])
+
+    return sorted_d
 
 
