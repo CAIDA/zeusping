1c4e450edd91221b662ce85b078a880f602165bb
Ramakrishna Padmanabhan <ramapad@caida.org>
2021-06-16 02:14:07 -0700

    Modified script to sample IR addresses for ZeusPing probing

--- find_addrs/select_iran_addrs.py
+++ find_addrs/select_iran_addrs.py
@@ -1,10 +1,23 @@
 
+# This is a script to select ~10M Iranian addresses out of 12.5M total
 
 import sys
 import random
+import os
 
-op_fname = sys.argv[1]
-op_fp = open(op_fname, 'w')
+op_fname_pref = sys.argv[1]
+num_splits = int(sys.argv[2]) # Number of files into which we should split the addresses
+
+mkdir_cmd = 'mkdir -p ./data/{0}'.format(op_fname_pref)
+sys.stderr.write("{0}\n".format(mkdir_cmd) )
+os.system(mkdir_cmd)
+
+op_fps = {}
+for sp in range(num_splits):
+    op_fname = "./data/{0}/{0}_numsp{1}_sp{2}".format(op_fname_pref, num_splits, sp+1) 
+    op_fps[sp] = open(op_fname, 'w')
+
+done_addrs = set()
 
 asn_to_addrs = {}
 
@@ -31,11 +44,17 @@ for line in sys.stdin:
 
 for asn in asn_to_addrs:
     for addr in asn_to_addrs[asn]:
-        num = random.randint(1, 2)
 
-        if num == 2:
-            op_fp.write("{0}\n".format(addr) )
+        num = random.randint(1, 5)
 
+        if ( (num != 5) and (addr not in done_addrs) ):
+
+            # Address needs to be sampled. Let's decide which file to write it to based on num_splits
+            file_num = random.randint(1, num_splits)
+
+            op_fps[file_num-1].write("{0}\n".format(addr) )
+            
+        done_addrs.add(addr)
         
     
     
