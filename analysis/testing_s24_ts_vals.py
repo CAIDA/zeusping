220edc7b54b3986a1a7f3c1003bd9cd9ef4702e2
Ramakrishna Padmanabhan <ramapad@caida.org>
2021-07-13 05:50:04 -0700

    WiP progress script that I've been using to test the multi-round analyses of s24 responses

--- analysis/testing_s24_ts_vals.py
+++ analysis/testing_s24_ts_vals.py
@@ -30,6 +30,9 @@ else:
 ip_fname = sys.argv[1]
 ip_fp = open(ip_fname)
 
+op_fp = open(sys.argv[2], 'w')
+
+# reqd_s24s_fname = sys.argv[3]
 
 def find_set_bits(s24, val):
 
@@ -49,20 +52,28 @@ def find_set_bits(s24, val):
     return s24_to_set_bits
 
 
+def write_addr_status(bit_string, status):
+    if bit_string > 0:
+        s24_to_set_bits = find_set_bits(s24, bit_string)
+        
+        for oct4 in s24_to_set_bits:
+            op_fp.write("{0} {1} {2}\n".format(oct4, s24_to_set_bits[oct4], status) )
+
+
 for line in ip_fp:
     parts = line.strip().split('|')
 
     s24 = parts[0].strip()
 
-    if s24 != '198.72.211.0/24':
+    if s24 != '24.30.248.0/24':
         continue
 
-    n_d = int(parts[1])
+    d_addrs = int(parts[1])
+    write_addr_status(d_addrs, 'd')
 
-    if n_d > 0:
-        s24_to_set_bits = find_set_bits(s24, n_d)
-        
-        for oct4 in s24_to_set_bits:
-            sys.stdout.write("{0} {1}\n".format(oct4, s24_to_set_bits[oct4]) )
+    r_addrs = int(parts[2])
+    write_addr_status(r_addrs, 'r')
 
-        sys.exit(1)
+    a_addrs = int(parts[3])
+    write_addr_status(a_addrs, 'a')
+    # sys.exit(1)
