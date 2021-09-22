0b9d27a8a74566bd88bb2820c8fbaabec0c214f0
Ramakrishna Padmanabhan <ramapad@caida.org>
2021-09-21 20:03:46 -0700

    Modified s24 testing script to work with the new format

--- analysis/s24/testing_s24_ts_vals.py
+++ analysis/s24/testing_s24_ts_vals.py
@@ -62,51 +62,33 @@ ip_fname = sys.argv[1]
 ip_fp = open(ip_fname)
 
 reqd_s24 = sys.argv[2]
-# op_fp = open(sys.argv[2], 'w')
 
-# reqd_s24s_fname = sys.argv[3]
+reqd_s24_addr_part = reqd_s24.strip().split('/')[0]
+# reqd_s24_addr_part = reqd_s24_parts[0]
 
-# I replaced this function with find_addrs_in_s24_with_status from zeusping_helpers.py
-# def find_set_bits(s24, val):
-
-
-#     s24_pref = s24[:-4]
-    
-#     s24_to_set_bits = defaultdict(int)
-#     curr_oct4 = 0
-#     for bit_pos in range(256):
-
-#         if( ( (val >> bit_pos) & 1) == 1):
-#             oct4 = "{0}{1}".format(s24_pref, curr_oct4)
-#             s24_to_set_bits[oct4] = 1
-
-#         curr_oct4 += 1
-
-#     return s24_to_set_bits
-
-
-        
+# Convert s24 file into ip addr
+reqd_s24_ipid = struct.unpack("!I", socket.inet_aton(reqd_s24_addr_part))[0]
 
 
 for line in ip_fp:
     parts = line.strip().split('|')
 
-    s24 = parts[0].strip()
+    s24 = int(parts[0].strip() )
 
-    if s24 != reqd_s24:
+    if s24 != reqd_s24_ipid:
     # if s24 != '142.202.88.0/24':
         continue
 
     s24_to_dets = defaultdict(set)
     
     d_addrs = int(parts[1])
-    zeusping_helpers.find_addrs_in_s24_with_status(s24, d_addrs, 'd', s24_to_dets)
+    zeusping_helpers.find_addrs_in_s24_with_status(reqd_s24, d_addrs, 'd', s24_to_dets)
     
     r_addrs = int(parts[2])
-    zeusping_helpers.find_addrs_in_s24_with_status(s24, r_addrs, 'r', s24_to_dets)
+    zeusping_helpers.find_addrs_in_s24_with_status(reqd_s24, r_addrs, 'r', s24_to_dets)
     
     a_addrs = int(parts[3])
-    zeusping_helpers.find_addrs_in_s24_with_status(s24, a_addrs, 'a', s24_to_dets)    
+    zeusping_helpers.find_addrs_in_s24_with_status(reqd_s24, a_addrs, 'a', s24_to_dets)    
     # sys.exit(1)
 
 statuses = ['d', 'r', 'a']
