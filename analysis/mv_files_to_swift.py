e294ae2ede92b0b5703a183c4c9c6e4655b37467
Ramakrishna Padmanabhan <ramapad@caida.org>
2021-09-30 16:27:28 -0700

    Refactored code foruploading files to Swift cluster

--- analysis/mv_files_to_swift.py
+++ analysis/mv_files_to_swift.py
@@ -1,3 +1,4 @@
+# TODO: Make compatible with Python3
 
 #  This software is Copyright (c) 2020 The Regents of the University of
 #  California. All Rights Reserved. Permission to copy, modify, and distribute this
@@ -40,57 +41,82 @@ import time
 import datetime
 import wandio
 
+
+def upload_file_to_swift_and_rm(fpath, fname, campaign, round_tstart, round_id):
+    round_tstart_dt = datetime.datetime.utcfromtimestamp(round_tstart)
+
+    file_to_write = fpath
+    object_name = 'datasource=zeusping/campaign={0}/year={1}/month={2}/day={3}/hour={4}/round={5}/{6}'.format(campaign, round_tstart_dt.year, round_tstart_dt.strftime("%m"), round_tstart_dt.strftime("%d"), round_tstart_dt.strftime("%H"), round_id, fname)
+    sys.stderr.write("File to upload to Swift: {0}\n".format(file_to_write) )
+
+    retval = 1
+    # Swift upload the file
+    try:
+        wandio.swift.upload(file_to_write, 'zeusping-processed', object_name)
+    except OSError:
+        sys.stderr.write("Wandio upload failed for {0}\n".format(file_to_write) )
+        retval = -1
+        return -1
+
+    if retval == 1:
+        # Delete the file, but *only* if upload was successful
+        os.remove(file_to_write)
+    
+    return retval
+    
+    
+def process_all_rounds(campaign, inp_dir, reqd_fnames, reqd_tstart, reqd_tend):
+
+    # List all the files in inp_dir
+    round_dirs = glob.glob('{0}/*_to_*'.format(inp_dir) )
+
+    for round_dir in round_dirs:
+
+        round_id = round_dir[len(inp_dir):]
+
+        round_tstart = int(round_id.strip().split('_')[0])
+        
+        if time.time() - round_tstart < (60 * MINUTE_BUFFER):
+            continue
+
+        if round_tstart < reqd_tstart or round_tstart >= reqd_tend:
+            continue
+
+        round_tend = round_tstart + 600
+        sys.stderr.write("{0}\n".format(round_id) )
+        
+        for fname in reqd_fnames:
+            fpath = "{0}/{1}".format(round_dir, fname)
+            retval = upload_file_to_swift_and_rm(fpath, fname, campaign, round_tstart, round_id)
+
+            if retval == -1:
+                # No big deal, continue to try to upload other files. We can always mop up the remaining files later.
+                pass
+            
+
+def obtain_reqd_fnames(reqd_fname):
+    reqd_fnames = []
+    if "," in reqd_fname:
+        parts = reqd_fname.strip().split(',')
+        for part in parts:
+            reqd_fnames.append(part)
+
+    return reqd_fnames
+
+    
 campaign = sys.argv[1]
 inp_dir = sys.argv[2]
 reqd_fname = sys.argv[3] # resps_per_addr, process_round etc.
+reqd_tstart = int(sys.argv[4])
+reqd_tend = int(sys.argv[5])
 
-# List all the files in inp_dir
-round_dirs = glob.glob('{0}/*_to_*'.format(inp_dir) )
+reqd_fnames = obtain_reqd_fnames(reqd_fname)
 
-for round_dir in round_dirs:
+# print(reqd_fnames)
+# sys.exit(1)
 
-    round_id = round_dir[len(inp_dir):]
-    print round_id
+# Specify how many minutes before the current minute we should refrain from processing
+MINUTE_BUFFER = 20
 
-    # sys.exit(1)
+process_all_rounds(campaign, inp_dir, reqd_fnames, reqd_tstart, reqd_tend)
 
-    round_tstart = int(round_id.strip().split('_')[0])
-    round_tend = round_tstart + 600
-    print round_tstart
-    print round_tend
-
-    round_tstart_dt = datetime.datetime.utcfromtimestamp(round_tstart)
-    
-    # sys.exit(1)
-
-    # Check if reqd_fname (resps_per_addr or resps_per_addr.gz) exists in this dir
-    fils = glob.glob('{0}/{1}*'.format(round_dir, reqd_fname) )
-
-    for fil in fils:
-
-        file_name = fil.strip().split('/')[-1]
-        
-        if fil[-2:] == 'gz':
-            object_name = 'datasource=zeusping/campaign={0}/year={1}/month={2}/day={3}/hour={4}/round={5}/{6}'.format(campaign, round_tstart_dt.year, round_tstart_dt.strftime("%m"), round_tstart_dt.strftime("%d"), round_tstart_dt.strftime("%H"), round_id, file_name)
-            file_to_write = fil
-        else:
-            # Compress the file
-            gzip_cmd = 'gzip {0}'.format(fil)
-            sys.stderr.write("{0}\n".format(gzip_cmd) )
-            args = shlex.split(gzip_cmd)
-
-            try:
-                subprocess.check_call(args)
-            except subprocess.CalledProcessError:
-                sys.stderr.write("Gzip failed for f {0}; exiting\n".format(fil) )
-                sys.exit(1)
-
-            object_name = 'datasource=zeusping/campaign={0}/year={1}/month={2}/day={3}/hour={4}/round={5}/{6}.gz'.format(campaign, round_tstart_dt.year, round_tstart_dt.strftime("%m"), round_tstart_dt.strftime("%d"), round_tstart_dt.strftime("%H"), round_id, file_name)
-            file_to_write = "{0}.gz".format(fil)
-
-
-        # Swift upload the file
-        wandio.swift.upload(file_to_write, 'zeusping-processed', object_name)
-            
-    
-    # sys.exit(1)
