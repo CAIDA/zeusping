7409028db097f435fec89200501ad3c9595c4763
Ramakrishna Padmanabhan <ramapad@caida.org>
2021-12-01 15:41:58 -0800

    Bug fixes

--- for_vps/prep_for_rsync.py
+++ for_vps/prep_for_rsync.py
@@ -2,11 +2,12 @@
 import sys
 import os
 import shlex
+import subprocess
 import glob
 import time
 import datetime
 import wandio
-
+import shutil
 
 def comp(fil, file_name):
     
@@ -50,7 +51,7 @@ def process_fil(fil, curr_time, until_time):
     # compression but before the mv, for example), it is possible that we may
     # find a compressed file in zp_output_dir. We will handle this edge-case
     # by using the was_compressed flag.
-    was_compressed = comp(fil, fil_name)
+    was_compressed = comp(fil, file_name)
 
     round_number = this_t/600
     round_tstart = round_number * 600
@@ -58,19 +59,29 @@ def process_fil(fil, curr_time, until_time):
 
     round_tstart_dt = datetime.datetime.utcfromtimestamp(round_tstart)
 
+    dst_fdir = '{0}/datasource=zeusping/campaign={1}/year={2}/month={3}/day={4}/hour={5}/'.format(rsync_dir, campaign, round_tstart_dt.year, round_tstart_dt.strftime("%m"), round_tstart_dt.strftime("%d"), round_tstart_dt.strftime("%H") )
+    mkdir_cmd = 'mkdir -p {0}/'.format(dst_fdir)
+    args = shlex.split(mkdir_cmd)
+    try:
+        subprocess.check_call(args)
+    except subprocess.CalledProcessError:
+        sys.stderr.write("Mkdir failed for {0}; exiting\n".format(mkdir_cmd) )
+        sys.exit(1)
+    
     if was_compressed == 1:
         # The file had already been compressed. No need to add the 'gz' extension
-        dst_fpath = '{0}/datasource=zeusping/campaign={1}/year={2}/month={3}/day={4}/hour={5}/{6}'.format(rsync_dir, campaign, round_tstart_dt.year, round_tstart_dt.strftime("%m"), round_tstart_dt.strftime("%d"), round_tstart_dt.strftime("%H"), file_name)
+        dst_fpath = '{0}/{1}'.format(dst_fdir, file_name)
         src_fpath = fil
     else:
         # We just compressed the file. Add the 'gz' extension
-        dst_fpath = '{0}/datasource=zeusping/campaign={1}/year={2}/month={3}/day={4}/hour={5}/{6}.gz'.format(rsync_dir, campaign, round_tstart_dt.year, round_tstart_dt.strftime("%m"), round_tstart_dt.strftime("%d"), round_tstart_dt.strftime("%H"), file_name)
+        dst_fpath = '{0}/{1}.gz'.format(dst_fdir, file_name)
         src_fpath = "{0}.gz".format(fil)
 
     try:
         shutil.move(src_fpath, dst_fpath)
+        sys.stderr.write("Successful mv for {0}, {1}\n".format(src_fpath, dst_fpath) )
     except:
-        sys.stderr.write("Failed mv for {0}\n".format(src_fpath) )
+        sys.stderr.write("Failed mv for {0}, {1}\n".format(src_fpath, dst_fpath) )
         sys.exit(1)
 
 
